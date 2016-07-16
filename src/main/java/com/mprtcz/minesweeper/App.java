5d7933d924984a1317423ca1cf35f97cdc27f742
Mordin <azet89@gmail.com>
2016-07-16 16:33:46 +0200

    refactored App file, modified pom .xml file to add maven dependencies

--- src/main/java/com/mprtcz/minesweeper/App.java
+++ src/main/java/com/mprtcz/minesweeper/App.java
@@ -7,15 +7,16 @@ public class App {
 
     public static void main(String[] args) {
         String testString = ".*..\n.*..\n....";
+        System.out.println("testString :\n" + testString);
         Minesweeper m = new MinesweeperImpl();
         m.setMineField(testString);
-        System.out.println(m.toString());
-        m.getHintField();
-        System.out.println(m.getHintField());
+        System.out.println("Output field: \n" +m.getHintField() +"\n");
 
 
         String testString1 = ".*.....\n.**....\n......*";
+
+        System.out.println("testString1 :\n" + testString1);
         m.setMineField(testString1);
-        System.out.println(m.getHintField());
+        System.out.println("Output field: \n" +m.getHintField() +"\n");
     }
 }
